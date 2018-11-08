07e5c4a252700eaef2c2b3d54781bf67c7e746e5
Bastien Paul <bastien.paul@coppernic.fr>
2018-11-08 15:56:24 +0100

    [BUILD] Make unit test pass

--- interactors/src/test/java/fr/coppernic/lib/interactors/barcode/BarcodeInteractorTest.java
+++ interactors/src/test/java/fr/coppernic/lib/interactors/barcode/BarcodeInteractorTest.java
@@ -3,6 +3,8 @@ package fr.coppernic.lib.interactors.barcode;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,11 +14,14 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.util.Collections;
+
 import fr.coppernic.lib.interactors.robolectric.RobolectricTest;
 import io.reactivex.disposables.Disposable;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class BarcodeInteractorTest extends RobolectricTest {
 
@@ -25,9 +30,13 @@ public class BarcodeInteractorTest extends RobolectricTest {
     private BarcodeInteractor interactor;
     @Mock
     private Context context;
+    @Mock
+    private PackageManager pm;
 
     @Before
     public void setUp() throws Exception {
+        when(context.getPackageManager()).thenReturn(pm);
+        when(pm.getInstalledPackages(PackageManager.GET_META_DATA)).thenReturn(Collections.<PackageInfo>emptyList());
         interactor = new BarcodeInteractor(context);
     }
 
